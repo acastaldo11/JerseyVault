7a20263c1d4b591d3a06c3a2073f68c07465307c
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-13 12:13:26 +0200

    Aggiunta classe Utente - versione iniziale

---
+++ src/main/java/model/Utente.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Utente {
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String passwordHash;
+
+    public Utente() {}
+}
