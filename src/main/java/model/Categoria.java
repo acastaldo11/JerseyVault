48245b8f133011a3d0734715f53b7da6be84d729
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-13 12:19:10 +0200

    Categoria: aggiunta descrizione, getter/setter, equals, hashCode e clone

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -1,8 +1,47 @@
 package model;
 
-public class Categoria {
+public class Categoria implements Cloneable {
+
     private int id;
     private String nome;
+    private String descrizione;
 
     public Categoria() {}
+
+    public Categoria(int id, String nome, String descrizione) {
+        this.id = id;
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescrizione() { return descrizione; }
+    public void setDescrizione(String descrizione) { this.descrizione = descrizione; }
+
+    @Override
+    public boolean equals(Object anObject) {
+        if (this == anObject) return true;
+        if (anObject == null || getClass() != anObject.getClass()) return false;
+        Categoria categoria = (Categoria) anObject;
+        return this.id == categoria.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 + id;
+    }
+
+    @Override
+    public Categoria clone() {
+        try {
+            return (Categoria) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
 }
