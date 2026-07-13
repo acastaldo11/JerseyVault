1978fab8774b5ff54615d5ccb1575a1dadbd0ed7
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-13 12:25:06 +0200

    Amministratore: aggiunta equals, hashCode e clone

--- src/main/java/model/Amministratore.java
+++ src/main/java/model/Amministratore.java
@@ -5,4 +5,23 @@ public class Amministratore extends Utente {
     public Amministratore() {
         super();
     }
+
+    public Amministratore(int id, String nome, String cognome, String email, String passwordHash) {
+        super(id, nome, cognome, email, passwordHash);
+    }
+
+    @Override
+    public boolean equals(Object anObject) {
+        return super.equals(anObject);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public Amministratore clone() {
+        return (Amministratore) super.clone();
+    }
 }
