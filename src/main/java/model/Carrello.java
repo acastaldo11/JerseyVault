d153c9338d7f8d2bb3b8e99298b59e5e17895773
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-14 12:00:23 +0200

    Carrello: aggiunta metodi aggiungi, rimuovi, aggiorna, svuota e getTotale

--- src/main/java/model/Carrello.java
+++ src/main/java/model/Carrello.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Carrello {
@@ -8,5 +9,49 @@ public class Carrello {
 
     public Carrello() {
         this.items = new ArrayList<>();
+    }
+
+    public List<CarrelloItem> getItems() {
+        return items;
+    }
+
+    public void aggiungi(Prodotto p, int quantita) {
+        for (CarrelloItem item : items) {
+            if (item.getProdotto().getId() == p.getId()) {
+                item.setQuantita(item.getQuantita() + quantita);
+                return;
+            }
+        }
+        items.add(new CarrelloItem(p, quantita));
+    }
+
+    public void rimuovi(int idProdotto) {
+        Iterator<CarrelloItem> it = items.iterator();
+        while (it.hasNext()) {
+            if (it.next().getProdotto().getId() == idProdotto) {
+                it.remove();
+            }
+        }
+    }
+
+    public void aggiorna(int idProdotto, int nuovaQuantita) {
+        for (CarrelloItem item : items) {
+            if (item.getProdotto().getId() == idProdotto) {
+                item.setQuantita(nuovaQuantita);
+                return;
+            }
+        }
+    }
+
+    public void svuota() {
+        items.clear();
+    }
+
+    public double getTotale() {
+        double totale = 0;
+        for (CarrelloItem item : items) {
+            totale += item.getPrezzoTotale();
+        }
+        return totale;
     }
 }
