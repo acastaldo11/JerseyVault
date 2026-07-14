e0d277d68094f609c5bc97f7c4b1e174f27c50b2
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-14 11:54:23 +0200

    MetodoPagamento: aggiunta tipo, ultimeCifre, getter/setter, equals e clone

--- src/main/java/model/MetodoPagamento.java
+++ src/main/java/model/MetodoPagamento.java
@@ -1,8 +1,52 @@
 package model;
 
-public class MetodoPagamento {
+public class MetodoPagamento implements Cloneable {
+
     private int id;
     private int idCliente;
+    private String tipo;
+    private String ultimeCifre;
 
     public MetodoPagamento() {}
+
+    public MetodoPagamento(int id, int idCliente, String tipo, String ultimeCifre) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.tipo = tipo;
+        this.ultimeCifre = ultimeCifre;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getIdCliente() { return idCliente; }
+    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public String getUltimeCifre() { return ultimeCifre; }
+    public void setUltimeCifre(String ultimeCifre) { this.ultimeCifre = ultimeCifre; }
+
+    @Override
+    public boolean equals(Object anObject) {
+        if (this == anObject) return true;
+        if (anObject == null || getClass() != anObject.getClass()) return false;
+        MetodoPagamento metodo = (MetodoPagamento) anObject;
+        return this.id == metodo.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 + id;
+    }
+
+    @Override
+    public MetodoPagamento clone() {
+        try {
+            return (MetodoPagamento) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
 }
