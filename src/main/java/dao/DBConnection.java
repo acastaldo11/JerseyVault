6306bb1c4fdecb737be5653f5c8e67c7f754e6f9
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-14 12:02:31 +0200

    DBConnection: aggiunta configurazione DataSource JNDI

--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -2,9 +2,28 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public class DBConnection {
+    private static DataSource ds;
+
+    static {
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            ds = (DataSource) envCtx.lookup("jdbc/jerseyvault");
+        } catch (NamingException e) {
+            System.err.println("Errore critico JNDI: " + e.getMessage());
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
-        return null;
+        if (ds == null) {
+            throw new SQLException("DataSource non inizializzato correttamente.");
+        }
+        return ds.getConnection();
     }
 }
