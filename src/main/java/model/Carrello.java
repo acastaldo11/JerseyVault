37c8d5bc97ec2436b6ac5d096be1a0591af51925
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-14 11:59:13 +0200

    Aggiunta classe Carrello - versione iniziale

---
+++ src/main/java/model/Carrello.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrello {
+    private List<CarrelloItem> items;
+
+    public Carrello() {
+        this.items = new ArrayList<>();
+    }
+}
