6df60bfd546e6b1fb4110a85a78323d0bc7644db
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-14 12:01:37 +0200

    Aggiunta classe DBConnection - versione iniziale

---
+++ src/main/java/dao/DBConnection.java
@@ -0,0 +1,10 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection getConnection() throws SQLException {
+        return null;
+    }
+}
