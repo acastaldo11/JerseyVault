efb262db94e9b1024ddcc2452135ac28ef0db957
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-15 12:11:47 +0200

    CheckEmailServlet: aggiunta verifica email con risposta JSON per AJAX

--- src/main/java/control/CheckEmailServlet.java
+++ src/main/java/control/CheckEmailServlet.java
@@ -1,11 +1,14 @@
 package control;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import dao.UtenteDAO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Cliente;
 
 @WebServlet("/checkEmail")
 public class CheckEmailServlet extends HttpServlet {
@@ -15,7 +18,22 @@ public class CheckEmailServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        String email = request.getParameter("email");
         response.setContentType("application/json");
-        response.getWriter().write("{\"disponibile\": true}");
+        response.setCharacterEncoding("UTF-8");
+
+        try {
+            UtenteDAO utenteDAO = new UtenteDAO();
+            Cliente cliente = utenteDAO.doRetrieveByEmail(email);
+
+            if (cliente != null) {
+                response.getWriter().write("{\"disponibile\": false}");
+            } else {
+                response.getWriter().write("{\"disponibile\": true}");
+            }
+        } catch (SQLException e) {
+            response.getWriter().write("{\"disponibile\": true}");
+        }
     }
 }
