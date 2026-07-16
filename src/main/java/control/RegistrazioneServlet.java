573c8ed16c592e2aa4f26c34cf4572c1022113e7
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-16 12:26:04 +0200

    Aggiunta RegistrazioneServlet - versione iniziale

---
+++ src/main/java/control/RegistrazioneServlet.java
@@ -0,0 +1,19 @@
+package control;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RegistrazioneServlet", urlPatterns = {"/registrazione"})
+public class RegistrazioneServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/view/registrazione.jsp").forward(request, response);
+    }
+}
