81fe160ae04a6314d12db284724f47116337428d
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-17 12:44:25 +0200

    refactor: AdminDashboardServlet

--- src/main/java/control/AdminDashboardServlet.java
+++ src/main/java/control/AdminDashboardServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 import dao.ProdottoDAO;
+import dao.ProdottoDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,21 +17,26 @@ import model.Prodotto;
 public class AdminDashboardServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    
+    private ProdottoDAO prodottoDAO;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        this.prodottoDAO = new ProdottoDAOImpl();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // Controllo e solo l'admin può accedere
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute("adminLoggato") == null) {
             response.sendRedirect(request.getContextPath() + "/admin/login");
             return;
         }
 
-        // Recupera  i prodotti e li manda alla JSP
         try {
-            ProdottoDAO prodottoDAO = new ProdottoDAO();
             List<Prodotto> prodotti = prodottoDAO.doRetrieveAll();
             request.setAttribute("prodotti", prodotti);
             request.getRequestDispatcher("/WEB-INF/view/admin/dashboard.jsp")
