6e148c80fe23349977f70666af26e91577c5a258
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-17 12:49:05 +0200

    dao: CategoriaDAOImpl

---
+++ src/main/java/dao/CategoriaDAOImpl.java
@@ -0,0 +1,63 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Categoria;
+
+public class CategoriaDAOImpl implements CategoriaDAO {
+
+    private static final String TABLE_NAME = "categoria";
+
+    @Override
+    public synchronized void doSave(Categoria categoria) throws SQLException {
+        String query = "INSERT INTO " + TABLE_NAME + " (nome_categoria, descrizione) VALUES (?, ?)";
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setString(1, categoria.getNome());
+            ps.setString(2, categoria.getDescrizione());
+            ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public synchronized Categoria doRetrieveById(int id) throws SQLException {
+        String query = "SELECT * FROM " + TABLE_NAME + " WHERE id = ?";
+        Categoria bean = null;
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement ps = connection.prepareStatement(query)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    bean = mapRowToCategoria(rs);
+                }
+            }
+        }
+        return bean;
+    }
+
+    @Override
+    public synchronized List<Categoria> doRetrieveAll() throws SQLException {
+        String query = "SELECT * FROM " + TABLE_NAME;
+        List<Categoria> categorie = new ArrayList<>();
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement ps = connection.prepareStatement(query);
+             ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
+                categorie.add(mapRowToCategoria(rs));
+            }
+        }
+        return categorie;
+    }
+
+    private Categoria mapRowToCategoria(ResultSet rs) throws SQLException {
+        Categoria c = new Categoria();
+        c.setId(rs.getInt("id"));
+        c.setNome(rs.getString("nome_categoria"));
+        c.setDescrizione(rs.getString("descrizione"));
+        return c;
+    }
+}
