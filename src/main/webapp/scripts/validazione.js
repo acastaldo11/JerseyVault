b0e7b0915882a59390ba57477e3818e171794d54
acastaldo11 <ac11.castaldo@gmail.com>
2026-07-20 16:41:27 +0200

    feat: aggiunta regex per controllo formato email e password

--- src/main/webapp/scripts/validazione.js
+++ src/main/webapp/scripts/validazione.js
@@ -1,9 +1,16 @@
 function validaFormRegistrazione() {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
+    
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let pwdRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-    if (email === "" || password === "") {
-        alert("Compila tutti i campi!");
+    if (!emailRegex.test(email)) {
+        console.log("Email non valida");
+        return false;
+    }
+    if (!pwdRegex.test(password)) {
+        console.log("Password troppo debole");
         return false;
     }
     return true;
